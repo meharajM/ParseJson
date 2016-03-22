b6ccde3a194f1dd5628cd95f3ae71aa2dbd081d1
meharaj <meharaj.m@snapwiz.com>
2016-03-22 11:43:35 +0530

    working condition

--- src/main/java/PaerseJson.java
+++ src/main/java/PaerseJson.java
@@ -4,23 +4,18 @@ import java.util.Iterator;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-//json package : com.googlecode.json-simple  add this to your projects's external libraries and you are ready to go
+
 
 class ParseJson{
 	public static void main(String args[]){
         JSONParser parser = new JSONParser();
 
         try {
-            Object obj = parser.parse(new FileReader("/home/meharaj/Desktop/styleSuggester/src/main/resources/cloths.json")); // get the file path and past it
+            Object obj = parser.parse(new FileReader("/home/meharaj/Desktop/styleSuggester/src/main/resources/cloths.json"));
             JSONObject jsonObject = (JSONObject) obj;
-            JSONArray categories = (JSONArray) jsonObject.keySet();
-            Iterator<JSONObject> iterator = categories.iterator();
-            while (iterator.hasNext()){
-                System.out.println(iterator.next());
-            }
 
-         //   System.out.println(jsonObject.keySet());
-           // System.out.println(jsonObject.values());
+            System.out.println(jsonObject.keySet());
+            System.out.println(jsonObject.values());
             /*Iterator<JSONObject> iterator = jsonObject.values().iterator();
             while (iterator.hasNext()){
                 JSONObject jsonchildObject = iterator.next();
